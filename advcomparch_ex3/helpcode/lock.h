b1c756dda109df3b9201f9b17dbeb08c31ceefa4
ilianamaggiori <ilianamag02@gmail.com>
2024-11-17 19:00:27 -0500

    Add files via upload

---
+++ advcomparch_ex3/helpcode/lock.h
@@ -0,0 +1,57 @@
+#ifndef LOCK_H_
+#define LOCK_H_
+
+typedef volatile int spinlock_t;
+
+#define UNLOCKED  0 
+#define LOCKED    1
+
+//initialization
+static inline void spin_lock_init(spinlock_t *spin_var)
+{
+    *spin_var = UNLOCKED; 
+}
+
+
+static inline void spin_lock_tas_cas(spinlock_t *spin_var)
+{
+    while (__sync_val_compare_and_swap(spin_var, UNLOCKED, LOCKED) != UNLOCKED);
+}
+
+static inline void spin_lock_ttas_cas(spinlock_t *spin_var)
+{
+    //We implement busy-waiting loop until the lock is acquired
+    while (1) {
+        if (*spin_var == UNLOCKED) {
+            if (__sync_val_compare_and_swap(spin_var, UNLOCKED, LOCKED) == UNLOCKED) {
+                break;
+            }
+        }
+        
+    }
+}
+
+static inline void spin_lock_tas_ts(spinlock_t *spin_var)
+{
+    while(__sync_lock_test_and_set(spin_var, LOCKED) == LOCKED);
+}
+
+static inline void spin_lock_ttas_ts(spinlock_t *spin_var)
+{
+    //We implement busy-waiting loop until the lock is acquired
+    while (1) {
+        if (*spin_var == UNLOCKED) {
+            if (__sync_lock_test_and_set(spin_var, LOCKED) == UNLOCKED) {
+                break;
+            }
+        }
+    }
+}
+//unlock the spinlock
+static inline void spin_unlock(spinlock_t *spin_var)
+{
+    __sync_lock_release(spin_var);
+}
+
+#endif
+
